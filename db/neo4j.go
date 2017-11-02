1fbb7c4ce485f1ff685b5ea57303d977416cbd74
David Balazs <david.balazs1993@ft.com>
2017-11-02 16:39:14 +0200

    Fixed query

--- db/neo4j.go
+++ db/neo4j.go
@@ -33,15 +33,16 @@ type Concept struct {
 
 func (s *NeoService) Read(conceptType string, conceptCh chan Concept) (int, bool, error) {
 	results := []Concept{}
-	stmt := `
+	stmt := fmt.Sprintf(`
 			MATCH (c:%s)-[:MENTIONS|MAJOR_MENTIONS|ABOUT|IS_CLASSIFIED_BY|IS_PRIMARILY_CLASSIFIED_BY|HAS_AUTHOR]-(cc:Content)
 			MATCH (c)-[:EQUIVALENT_TO]->(x:Thing)
 			RETURN DISTINCT x.prefUUID AS Uuid, x.prefLabel AS PrefLabel, labels(c) AS Labels
-		`
+		`,conceptType)
+
 	if conceptType == "Organisation" {
 		stmt = `
-		MATCH (content:Content)-[rel:MENTIONS|MAJOR_MENTIONS|ABOUT|IS_CLASSIFIED_BY|IS_PRIMARILY_CLASSIFIED_BY|HAS_AUTHOR]->(concept:%s)
-		OPTIONAL MATCH (concept)-[:EQUIVALENT_TO]->(x:%s)
+		MATCH (content:Content)-[rel:MENTIONS|MAJOR_MENTIONS|ABOUT|IS_CLASSIFIED_BY|IS_PRIMARILY_CLASSIFIED_BY|HAS_AUTHOR]->(concept:Organisation)
+		OPTIONAL MATCH (concept)-[:EQUIVALENT_TO]->(x:Organisation)
 		OPTIONAL MATCH (concept)<-[:IDENTIFIES]-(factset:FactsetIdentifier)
 		OPTIONAL MATCH (concept)<-[:IDENTIFIES]-(lei:LegalEntityIdentifier)
 		OPTIONAL MATCH (concept)<-[:ISSUED_BY]-(:FinancialInstrument)<-[:IDENTIFIES]-(figi:FIGIIdentifier)
@@ -51,15 +52,15 @@ func (s *NeoService) Read(conceptType string, conceptCh chan Concept) (int, bool
 	}
 	if conceptType == "Person" {
 		stmt = `
-		MATCH (content:Content)-[rel:MENTIONS|MAJOR_MENTIONS|ABOUT|IS_CLASSIFIED_BY|IS_PRIMARILY_CLASSIFIED_BY|HAS_AUTHOR]->(concept:%s)
-		OPTIONAL MATCH (concept)-[:EQUIVALENT_TO]->(x:%s)
+		MATCH (content:Content)-[rel:MENTIONS|MAJOR_MENTIONS|ABOUT|IS_CLASSIFIED_BY|IS_PRIMARILY_CLASSIFIED_BY|HAS_AUTHOR]->(concept:Person)
+		OPTIONAL MATCH (concept)-[:EQUIVALENT_TO]->(x:Person)
 		RETURN DISTINCT coalesce(x.prefUUID, concept.uuid) as Uuid, coalesce(labels(x), labels(concept)) as Labels,
                 coalesce(x.prefLabel, concept.prefLabel) as PrefLabel
 		`
 	}
 
 	query := &neoism.CypherQuery{
-		Statement: fmt.Sprintf(stmt, conceptType),
+		Statement:    stmt,
 		Result:       &results,
 	}
 
